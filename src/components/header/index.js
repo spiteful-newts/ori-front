db570c0240b94d451590b7e4684a4c94d1cce609
pure-js <3690368+pure-js@users.noreply.github.com>
2021-04-10 20:38:45 +0300

    chore: everything to spaces

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,24 +4,24 @@ import { Link } from 'preact-router/match';
 import style from './style.css';
 
 const Header = () => {
-	const [contacts, setContacts] = useState(null);
-	useEffect(() => {
-		fetch('http://localhost:1337/contacts')
-			.then((data) => data.json())
-			.then((data) => {
-				setContacts(data);
-			});
-	}, []);
-	return (
-	<header class={style.header}>
-		<h1>Preact App</h1>
-		<nav>
-			<Link activeClassName={style.active} href="/">Home</Link>
-			<p>{ contacts?.email }</p>
-			<Link activeClassName={style.active} href="/profile">Me</Link>
-			<Link activeClassName={style.active} href="/profile/john">John</Link>
-		</nav>
-	</header>
+  const [contacts, setContacts] = useState(null);
+  useEffect(() => {
+    fetch('http://localhost:1337/contacts')
+      .then((data) => data.json())
+      .then((data) => {
+        setContacts(data);
+      });
+  }, []);
+  return (
+  <header class={style.header}>
+    <h1>Preact App</h1>
+    <nav>
+      <Link activeClassName={style.active} href="/">Home</Link>
+      <p>{ contacts?.email }</p>
+      <Link activeClassName={style.active} href="/profile">Me</Link>
+      <Link activeClassName={style.active} href="/profile/john">John</Link>
+    </nav>
+  </header>
 )};
 
 export default Header;
